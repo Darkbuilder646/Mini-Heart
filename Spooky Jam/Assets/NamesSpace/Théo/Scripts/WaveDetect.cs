847105a71c3e72a4927885edf9e0e96d25c84229
Lejeune <theolejeune.tl@gmail.com>
2022-10-22 12:29:27 +0200

    Ennemi wave v1

---
+++ Spooky Jam/Assets/NamesSpace/Théo/Scripts/WaveDetect.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveDetect : MonoBehaviour
+{
+    private EnnemiWave ennemiWaveScript;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if(other.TryGetComponent<EnnemiWave>(out ennemiWaveScript))
+        ennemiWaveScript.StartWave();
+        Debug.Log("Collider : " + other.name);
+    }
+
+}
