9bef937f6f3dbd8ce494c7c84d043558c695c96f
iesnihS <fullcandies@gmail.com>
2022-10-22 11:08:33 +0200

    Character Controler Works ^^

--- Spooky Jam/Assets/Scripts/Character/CharacterControler.cs
+++ Spooky Jam/Assets/Scripts/Character/CharacterControler.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class CharacterControler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region SerializeField
+    [SerializeField] private float Speed = 1;
+    [SerializeField] private PlayerInput playerInput = null;
+    [SerializeField, Range(0,1), Tooltip("0.05")] private float AccelerationPower = 0.2f;
+    #endregion
+
+    #region private
+    private AccelerationType AccelerationCurrentMode = AccelerationType.None;
+    private float AccelerationIndex = 0;
+    private enum AccelerationType
     {
-        
+        AccelerationFinished,
+        DecelerationFinished,
+        None
+    }
+    private Vector3 Direction;
+    #endregion
+    private void FixedUpdate()
+    {
+        if (playerInput.GetDirection() != Vector3.zero)
+        {
+            Acceleration(AccelerationType.AccelerationFinished);
+        }
+        else
+        {
+            Acceleration(AccelerationType.DecelerationFinished);
+        }
+
+        transform.position += Direction * Time.deltaTime * Speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Acceleration(AccelerationType type)
     {
-        
+        AccelerationIndex = AccelerationCurrentMode != type ? 0 : AccelerationIndex;
+        AccelerationIndex = Mathf.Clamp(AccelerationIndex + AccelerationPower, 0, 1);
+        Direction = Vector3.Lerp(Direction, playerInput.GetDirection().normalized, AccelerationIndex);
+        AccelerationCurrentMode = type;
     }
 }
--- Spooky Jam/Assets/Scripts/Character/PlayerInput.cs
+++ Spooky Jam/Assets/Scripts/Character/PlayerInput.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 Direction;
+    private void Update()
     {
-        
+        Direction = Vector3.zero;
+        Direction += Input.GetKey(KeyCode.Z) ? Vector3.up : Vector3.zero;
+        Direction += Input.GetKey(KeyCode.Q) ? Vector3.left : Vector3.zero;
+        Direction += Input.GetKey(KeyCode.D) ? Vector3.right : Vector3.zero;
+        Direction += Input.GetKey(KeyCode.S) ? Vector3.down : Vector3.zero;
     }
-
-    // Update is called once per frame
-    void Update()
+    public Vector3 GetDirection()
     {
-        
+        return Direction;
     }
 }
