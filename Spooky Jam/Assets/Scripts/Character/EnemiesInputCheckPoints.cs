ec78c1879e81cd4f428f528a72b09f632367dc67
iesnihS <fullcandies@gmail.com>
2022-10-22 15:33:28 +0200

    Characters

--- Spooky Jam/Assets/Scripts/Character/EnemiesInputCheckPoints.cs
+++ Spooky Jam/Assets/Scripts/Character/EnemiesInputCheckPoints.cs
@@ -2,27 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemiesInputCheckPoints : MonoBehaviour , InputInterface
+public class EnemiesInputCheckPoints : MonoBehaviour, InputInterface
 {
     private enum ModeEnemy
     {
         Chase,
-        Shearch,
+        Search,
         Checkpoint
     }
     [SerializeField] private List<Vector3> CheckPoints;
     [SerializeField] private float DistanceFinish = 0.2f;
+    [SerializeField] private float TimeSearch = 1;
     private int index;
     private Vector3 Direction;
-    private bool OnChase = false;
+    private ModeEnemy CurrentMode = ModeEnemy.Checkpoint;
     private GameObject CurrentPlayer;
+    private float Timer;
 
     private void Awake()
     {
-        for(int i = 0; i<CheckPoints.Count; i++)
-        {
-            CheckPoints[i] = transform.TransformPoint(CheckPoints[i]);
-        }
+        Timer = TimeSearch;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -30,7 +29,7 @@ public class EnemiesInputCheckPoints : MonoBehaviour , InputInterface
         if (collision.gameObject.TryGetComponent<PlayerInput>(out p))
         {
             CurrentPlayer = p.gameObject;
-            OnChase = true;
+            CurrentMode = ModeEnemy.Chase;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -39,12 +38,12 @@ public class EnemiesInputCheckPoints : MonoBehaviour , InputInterface
         if (collision.gameObject.TryGetComponent<PlayerInput>(out p))
         {
             CurrentPlayer = p.gameObject;
-            OnChase = false;
+            CurrentMode = ModeEnemy.Search;
         }
     }
     private void Update()
     {
-        if (!OnChase)
+        if (CurrentMode == ModeEnemy.Checkpoint)
         {
             Direction = CheckPoints[index] - transform.position;
             float Distance = Direction.magnitude;
@@ -53,9 +52,22 @@ public class EnemiesInputCheckPoints : MonoBehaviour , InputInterface
         }
         else
         {
+            if (CurrentMode == ModeEnemy.Search)
+            {
+                Timer -= Time.deltaTime;
+                if (Timer <= 0)
+                {
+                    CurrentMode = ModeEnemy.Checkpoint;
+                    Timer = TimeSearch;
+                }
+            }
+            else
+            {
+                Timer = TimeSearch;
+            }
             Direction = CurrentPlayer.transform.position - transform.position;
         }
-        
+
     }
     public List<Vector3> checkPoints { get => CheckPoints; set => CheckPoints = value; }
 
