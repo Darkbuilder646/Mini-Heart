dc0f459b1caf35d708a5f937df87861a1f4d3a2f
Lejeune <theolejeune.tl@gmail.com>
2022-10-22 13:52:08 +0200

    Beat du coeur

--- Spooky Jam/Assets/NamesSpace/Théo/Scripts/Beat.cs
+++ Spooky Jam/Assets/NamesSpace/Théo/Scripts/Beat.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Beat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject particlePrefab;
+    [SerializeField] private List<AudioClip> beatSound = new List<AudioClip>();
+    [SerializeField] private float delay = 2;
+    private bool isActive = false;
+
+    private void Start() 
     {
-        
+        StartCoroutine(PlayBeat());
+    }
+    private void Update() 
+    {
+        if(!isActive)
+        {
+            StartCoroutine(PlayBeat());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [ContextMenu("Init Beat")]
+    IEnumerator PlayBeat()
     {
-        
+        isActive = true;
+        Instantiate(particlePrefab, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(delay);
+        isActive = false;
     }
+
+
+
+    /*
+    //TODO : Préfab particule systeme
+    //TODO : Init le préfab à chaque beats au coordonnée du joueur
+    TODO : Play l'audio clip du beat 
+    //TODO : destroy l'ancien beat avant d'en créer un nouveau
+    */
 }
