5955da7ee805423c70f72264f9aa4b90eba547d6
iesnihS <fullcandies@gmail.com>
2022-10-23 18:24:52 +0200

    Oui

--- Spooky Jam/Assets/NamesSpace/Théo/Scripts/Heart/KeyAndDoor.cs
+++ Spooky Jam/Assets/NamesSpace/Théo/Scripts/Heart/KeyAndDoor.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyAndDoor : MonoBehaviour
 {
     [SerializeField] private bool asRedKey = false;
     [SerializeField] private bool asBlueKey = false;
+    [SerializeField] private Image KeyRedImage;
+    [SerializeField] private Image KeyBlueImage;
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.CompareTag("RedKey")) 
         {
+            KeyRedImage.canvas.enabled = true;
+            Debug.Log("RedKey");
             asRedKey = true;
+            
+            
             Destroy(other.gameObject);
         }
         if(other.CompareTag("BlueKey"))
         {
             asBlueKey = true;
+            KeyBlueImage.canvas.enabled = true;
             Destroy(other.gameObject);
         }
         
@@ -27,14 +35,14 @@ public class KeyAndDoor : MonoBehaviour
         if(other.collider.CompareTag("RedDoor") && asRedKey)
         {
             asRedKey = false;
-            Debug.Log("Red Door Unlock");
+            KeyRedImage.canvas.enabled = false;
             Destroy(other.gameObject);
 
         }
         if(other.collider.CompareTag("BlueDoor") && asBlueKey)
         {
             asBlueKey = false;
-            Debug.Log("Blue Door Unlock");
+            KeyBlueImage.canvas.enabled = false;
             Destroy(other.gameObject);
 
         }
