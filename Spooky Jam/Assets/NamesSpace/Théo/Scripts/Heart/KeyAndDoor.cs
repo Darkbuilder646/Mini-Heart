b1beb67ba6172f0fa945f5446db9fa387739e812
Lejeune <theolejeune.tl@gmail.com>
2022-10-23 13:04:55 +0200

    End Level

--- Spooky Jam/Assets/NamesSpace/Théo/Scripts/Heart/KeyAndDoor.cs
+++ Spooky Jam/Assets/NamesSpace/Théo/Scripts/Heart/KeyAndDoor.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class KeyAndDoor : MonoBehaviour
 {
+    [SerializeField] private GameObject prefabOpenDoor;
     [SerializeField] private bool asRedKey = false;
     [SerializeField] private bool asBlueKey = false;
 
@@ -30,6 +31,7 @@ public class KeyAndDoor : MonoBehaviour
         {
             asRedKey = false;
             Debug.Log("Red Door Unlock");
+            
             Destroy(other.gameObject);
 
         }
@@ -37,6 +39,7 @@ public class KeyAndDoor : MonoBehaviour
         {
             asBlueKey = false;
             Debug.Log("Blue Door Unlock");
+            
             Destroy(other.gameObject);
 
         }
